64deb95765d3771b2a925873bc88e4c93b5dae8d
gurrium <gurrium@hatena.ne.jp>
2025-04-14 15:41:02 +0900

    initial commit

---
+++ ReproduceCrash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReproduceCrash
+//
+//  Created by gurrium on 2025/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReproduceCrash/ReproduceCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReproduceCrashApp.swift
+//  ReproduceCrash
+//
+//  Created by gurrium on 2025/04/14.
+//
+
+import SwiftUI
+
+@main
+struct ReproduceCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
